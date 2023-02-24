777f694e795307d423ce34ffdbe770fc2510bc1f
Henry <Henryjvp24@hotmail.com>
2023-02-24 13:01:23 +0000

    modificamos el formulario, agregamos mas opciones

--- resources/views/Productos/agregar.blade.php
+++ resources/views/Productos/agregar.blade.php
@@ -7,11 +7,39 @@
 
   <div class="bg-slate-400 h-full ">
     <form action="" method="post">
-      <input type="text" name="nombre">
-      <input type="text" name="descripcion">
-      <input type="text" name="precio">
-      <input type="text" name="cantidadDisponible">
-  
+
+      <input type="radio" name="producto" vadlue="playera" class="shirt">Playera
+      <input type="radio" name="producto" value="sudadera" class="shirt">Sudadera
+      <input type="radio" name="producto" value="tazas" class="others">Tazas
+      <input type="radio" name="producto" value="gorras" class="others">Gorras
+      <br>
+      <div id="playera_sudadera" class="hidden">
+        {{-- tallas unicamente se mostraran con las playeras o sudaderas --}}
+        <input type="radio" name="talla" value="xs">XS
+        <input type="radio" name="talla" value="s">S
+        <input type="radio" name="talla" value="m">M
+        <input type="radio" name="talla" value="l">L
+        <br>
+        {{-- colores unicamente se mostraran con las playeras o sudaderas --}}
+        <input type="radio" name="color" value="blanco">Blanco
+        <input type="radio" name="color" value="Negro">Negro
+        <input type="radio" name="color" value="GrisJaspe">Gris Jaspe
+        <input type="radio" name="color" value="NegroJaspe">Negro Jaspe
+        <input type="radio" name="color" value="Royal">Royal
+        <input type="radio" name="color" value="VerdeBosque">Verde Bosque
+        <input type="radio" name="color" value="Marron">Marron
+        <input type="radio" name="color" value="Rojo">Rojo
+        <input type="radio" name="color" value="Marino">Marino
+        <br>
+        {{-- genero unicamente se mostraran con las playeras o sudaderas --}}
+        <input type="radio" name="genero" value="Dama">Dama
+        <input type="radio" name="genero" value="Dama">Caballero
+      </div>
+      
+      <input type="text" name="price" placeholder="precio">
+
+      <input type="text" name="cantidad" placeholder="cantidad">
+
       <input type="submit" value="Agregar">
     </form>
   </div>
