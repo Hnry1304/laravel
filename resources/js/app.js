a140ef7f191cb867ce2dd916db53712b02b22573
Henry <Henryjvp24@hotmail.com>
2023-02-24 13:00:54 +0000

    Agregamos funcion para esconder opciones del formulario

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,32 @@
-import './bootstrap';
+// import './bootstrap';
+
+let playera = document.querySelectorAll(".shirt");
+const enableShirtOption = document.getElementById('playera_sudadera');
+
+let otrosProducto = document.querySelectorAll(".others");
+
+
+for(let i= 0; i < playera.length; i++){
+  playera[i].addEventListener('click', () =>{
+    
+    if(enableShirtOption.classList.contains('hidden')){
+      enableShirtOption.classList.toggle('hidden');  
+    }
+  });
+}
+
+for(let i= 0; i < otrosProducto.length; i++){
+  otrosProducto[i].addEventListener('click', () =>{
+    
+    if(!enableShirtOption.classList.contains('hidden')){
+      enableShirtOption.classList.toggle('hidden');  
+    }
+  });
+}
+/*
+playera.addEventListener('click', ()=>{
+  enableShirtOption.classList.toggle('hidden');
+
+});
+
+*/
