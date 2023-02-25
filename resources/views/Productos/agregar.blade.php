2cbaf705eedcbcf9e540bcae32af22ddbf7d7f56
Henry <Henryjvp24@hotmail.com>
2023-02-25 14:56:50 +0000

    agregamos ruta donde se enviara los datos

--- resources/views/Productos/agregar.blade.php
+++ resources/views/Productos/agregar.blade.php
@@ -6,9 +6,9 @@
   </button>
 
   <div class="bg-slate-400 h-full ">
-    <form action="" method="post">
-
-      <input type="radio" name="producto" vadlue="playera" class="shirt">Playera
+    <form action="agregarProducto" method="post">
+      @csrf
+      <input type="radio" name="producto" value="playera" class="shirt">Playera
       <input type="radio" name="producto" value="sudadera" class="shirt">Sudadera
       <input type="radio" name="producto" value="tazas" class="others">Tazas
       <input type="radio" name="producto" value="gorras" class="others">Gorras
@@ -19,6 +19,9 @@
         <input type="radio" name="talla" value="s">S
         <input type="radio" name="talla" value="m">M
         <input type="radio" name="talla" value="l">L
+        @error('talla')
+          <p> {{$message}} </p>
+        @enderror
         <br>
         {{-- colores unicamente se mostraran con las playeras o sudaderas --}}
         <input type="radio" name="color" value="blanco">Blanco
@@ -30,17 +33,29 @@
         <input type="radio" name="color" value="Marron">Marron
         <input type="radio" name="color" value="Rojo">Rojo
         <input type="radio" name="color" value="Marino">Marino
+        @error('color')
+          <p> {{$message}} </p>
+        @enderror
         <br>
         {{-- genero unicamente se mostraran con las playeras o sudaderas --}}
         <input type="radio" name="genero" value="Dama">Dama
         <input type="radio" name="genero" value="Dama">Caballero
+        @error('genero')
+          <p> {{$message}} </p>
+        @enderror`
       </div>
       
-      <input type="text" name="price" placeholder="precio">
-
+      <input type="text" name="precio" placeholder="precio">
+      @error('precio')
+        <p> {{$message}} </p>
+      @enderror
+      
       <input type="text" name="cantidad" placeholder="cantidad">
-
+      @error('cantidad')
+        <p> {{$message}} </p>
+      @enderror
       <input type="submit" value="Agregar">
+      
     </form>
   </div>
 
