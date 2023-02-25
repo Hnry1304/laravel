d85d31470a468eaa559fbc8bad72333753b9e289
Henry <Henryjvp24@hotmail.com>
2023-02-25 14:55:30 +0000

    funcion datos, recibe los datos enviados desde un formulario

--- app/Http/Controllers/ProductosController.php
+++ app/Http/Controllers/ProductosController.php
@@ -10,4 +10,26 @@ class ProductosController extends Controller
 
         return view('Productos.agregar');
     }
+
+    public function datos(Request $request){
+        $producto = $request->input('producto');
+
+        if($producto == 'sudadera' || $producto == 'playera'){
+            $info = [
+                'talla' => 'required',
+                'color' => 'required',
+                'genero' => 'required',
+                'precio' => 'required',
+                'cantidad' => 'required',
+            ];
+        }else{
+            $info = [
+                'precio' => 'required',
+                'cantidad' => 'required',
+            ];
+        }
+
+        $validated = $request->validate($info);
+            
+    }
 }
