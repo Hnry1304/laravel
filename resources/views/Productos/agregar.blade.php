d612836e612512fce0402479bfe13704384b16e6
Henry <Henryjvp24@hotmail.com>
2023-02-26 14:37:12 +0000

    Agregamos old en las vistas

--- resources/views/Productos/agregar.blade.php
+++ resources/views/Productos/agregar.blade.php
@@ -8,41 +8,41 @@
   <div class="bg-slate-400 h-full ">
     <form action="agregarProducto" method="post">
       @csrf
-      <input type="radio" name="producto" value="playera" class="shirt">Playera
-      <input type="radio" name="producto" value="sudadera" class="shirt">Sudadera
+      <input type="radio" name="producto" value="playera" class="shirt" {{ old('producto') == 'playera' ? 'checked='.' '.'checked' .'' : ' '}}>Playera
+      <input type="radio" name="producto" value="sudadera" class="shirt" {{ old('producto') == 'sudadera' ? 'checked='.' '.'checked' .'' : ' '}}>Sudadera
       <input type="radio" name="producto" value="tazas" class="others">Tazas
       <input type="radio" name="producto" value="gorras" class="others">Gorras
       <br>
       <div id="playera_sudadera" class="hidden">
         {{-- tallas unicamente se mostraran con las playeras o sudaderas --}}
-        <input type="radio" name="talla" value="xs">XS
-        <input type="radio" name="talla" value="s">S
-        <input type="radio" name="talla" value="m">M
-        <input type="radio" name="talla" value="l">L
+        <input type="radio" name="talla" value="xs" {{ old('talla') == 'xs' ? 'checked='.' '.'checked' .'' : ' '}}>XS
+        <input type="radio" name="talla" value="s" {{ old('talla') == 's' ? 'checked='.' '.'checked' .'' : ' '}}>S
+        <input type="radio" name="talla" value="m" {{ old('talla') == 'm' ? 'checked='.' '.'checked' .'' : ' '}}>M
+        <input type="radio" name="talla" value="l" {{ old('talla') == 'l' ? 'checked='.' '.'checked' .'' : ' '}}>L
         @error('talla')
           <p> {{$message}} </p>
         @enderror
         <br>
         {{-- colores unicamente se mostraran con las playeras o sudaderas --}}
-        <input type="radio" name="color" value="blanco">Blanco
-        <input type="radio" name="color" value="Negro">Negro
-        <input type="radio" name="color" value="GrisJaspe">Gris Jaspe
-        <input type="radio" name="color" value="NegroJaspe">Negro Jaspe
-        <input type="radio" name="color" value="Royal">Royal
-        <input type="radio" name="color" value="VerdeBosque">Verde Bosque
-        <input type="radio" name="color" value="Marron">Marron
-        <input type="radio" name="color" value="Rojo">Rojo
-        <input type="radio" name="color" value="Marino">Marino
+        <input type="radio" name="color" value="Blanco" {{ old('color') == 'Blanco' ? 'checked='.' '.'checked' .'' : ' '}}>Blanco
+        <input type="radio" name="color" value="Negro" {{ old('color') == 'Negro' ? 'checked='.' '.'checked' .'' : ' '}}>Negro
+        <input type="radio" name="color" value="GrisJaspe" {{ old('color') == 'GrisJaspe' ? 'checked='.' '.'checked' .'' : ' '}}>Gris Jaspe
+        <input type="radio" name="color" value="NegroJaspe"{{ old('color') == 'NegroJaspe' ? 'checked='.' '.'checked' .'' : ' '}}>Negro Jaspe
+        <input type="radio" name="color" value="Royal" {{ old('color') == 'Royal' ? 'checked='.' '.'checked' .'' : ' '}}>Royal
+        <input type="radio" name="color" value="VerdeBosque" {{ old('color') == 'VerdeBosque' ? 'checked='.' '.'checked' .'' : ' '}}>Verde Bosque
+        <input type="radio" name="color" value="Marron" {{ old('color') == 'Marron' ? 'checked='.' '.'checked' .'' : ' '}}>Marron
+        <input type="radio" name="color" value="Rojo" {{ old('color') == 'Rojo' ? 'checked='.' '.'checked' .'' : ' '}}>Rojo
+        <input type="radio" name="color" value="Marino" {{ old('color') == 'Marino' ? 'checked='.' '.'checked' .'' : ' '}}>Marino
         @error('color')
           <p> {{$message}} </p>
         @enderror
         <br>
         {{-- genero unicamente se mostraran con las playeras o sudaderas --}}
-        <input type="radio" name="genero" value="Dama">Dama
-        <input type="radio" name="genero" value="Dama">Caballero
+        <input type="radio" name="genero" value="Dama" {{ old('genero') == 'Dama' ? 'checked='.' '.'checked' .'' : ' '}}>Dama
+        <input type="radio" name="genero" value="Caballero" {{ old('genero') == 'Caballero' ? 'checked='.' '.'checked' .'' : ' '}}>Caballero
         @error('genero')
           <p> {{$message}} </p>
-        @enderror`
+        @enderror
       </div>
       
       <input type="text" name="precio" placeholder="precio">
